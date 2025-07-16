0de2a602b9c0df505f1c844f2be99e56110e4368
2ndoneda <aasthachandankar@gmail.com>
2025-07-16 13:12:45 +0530

    Add Firebase to client and create config

---
+++ client/src/firebaseConfig.ts
@@ -0,0 +1,20 @@
+// src/firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3wZTanCdGxG6jpo39CkqUcM9LhK17BME",
+  authDomain: "ajnabicam.firebaseapp.com",
+  projectId: "ajnabicam",
+  storageBucket: "ajnabicam.appspot.com",
+  messagingSenderId: "558188110620",
+  appId: "1:558188110620:web:500cdf55801d5b00e9d0d9",
+  measurementId: "G-XM2WK7W95Q"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
+// Analytics only works in HTTPS / production
+export const analytics = await isSupported().catch(() => false) 
+  ? getAnalytics(firebaseApp) 
+  : null;
