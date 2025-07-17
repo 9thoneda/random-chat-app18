9c6d0d1e22fbc748e717242e5b4cdfedb9f1d353
9thoneda <khushispandora@gmail.com>
2025-07-17 17:31:04 +0530

    Updated Home.tsx

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -266,7 +266,7 @@ export default function Home() {
           </div>
 
           {/* Enhanced Carousel Dots */}
-          <div className="absolute bottom-1 right-1 flex gap-1">
+          <div className="absolute bottom-1 right-6 flex gap-1">
             {bannerImages.map((_, index) => (
               <button
                 key={index}
